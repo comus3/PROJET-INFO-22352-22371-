ddba757ca314f10d4ca496abf572ee1685a3fc65
zekhayoub <22371@ecam.be>
2023-03-27 16:41:34 +0200

    random model added(still a bit buggy)

--- utils.py
+++ utils.py
@@ -65,7 +65,31 @@ def validMoves(status,ianame):
         validPositions.append(playerPos+1)
     validPositions.append(playerPos)
     return validPositions
-        
+
+
+def findtreasure(plateau):
+    def rdfs(i, j, chemin, visites):   #Méthode qui est utilisé Recursive Depth-first search    
+        chemin.append((i, j))
+        if plateau[i][j].statue == "trésor":
+            positions_tresors.append((i, j))
+        visites.add((i, j))
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for di, dj in directions:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < len(plateau) and 0 <= nj < len(plateau[0]) and plateau[ni][nj].statue != "board" and (ni, nj) not in visites:
+                rdfs(ni, nj, chemin, visites)
+        chemin.pop()
+        visites.remove((i, j))
+
+    positions_tresors = []  # liste des positions de trésor
+    visites = set()  # c'est l'ensemble des cases visitées pour que ça évite les boucles infinies
+    for i in range(len(plateau)):
+        for j in range(len(plateau[0])):
+            if plateau[i][j].statue == "start":
+                rdfs(i, j, [], visites)
+    return positions_tresors
+
+
 
 ##############################################          ALL CREDITS TO LURK1        ######################################################
 
