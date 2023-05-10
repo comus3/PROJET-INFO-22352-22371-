f3a6341a6cdd20ee5fbe632169b21cb526a274e1
comus3 <come.plantin.carrenard@gmail.com>
2023-05-10 16:33:57 +0200

    Finished model testing

--- test_utils.py
+++ test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 from main import *
 from utils import *
+from collections import deque
 
 
 
@@ -11,7 +12,62 @@ False, 'item': None}, {'N': True, 'E': True, 'S': False, 'W': True, 'item': 10},
 
 
 
+def path(start, end, board):
+    def successors(index):
+        res = []
+        for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            coords = add(index2coords(index), dir)
+            dirName = DIRECTIONS[dir]["name"]
+            opposite = DIRECTIONS[dirName]["opposite"]
+            # breakpoint()
+            if isCoordsValid(*coords):
+                if board[index][dirName] and board[coords2index(*coords)][opposite]:
+                    res.append(coords2index(*coords))
+        return res
+    try:
+        res = BFS(start, successors, [end])
+        print(res)
+        return res
+    except IndexError:
+        return None
+    
+def BFS(start, successors, goals):
+    q = deque()
+    parent = {}
+    parent[start] = None
+    node = start
+    while node not in goals:
+        for successor in successors(node):
+            if successor not in parent:
+                parent[successor] = node
+                q.append(successor)
+        node = q.popleft()
 
+    res = []
+    while node is not None:
+        res.append(node)
+        node = parent[node]
+
+    return list(reversed(res))
+
+
+def isValid(move,state,newState):
+    if not isSameTile(newState["tile"], move["tile"]):
+        return False
+    if move["gate"] not in "ABCDEFGHIJKL":
+        return False
+    if move["new_position"] not in range(49):
+        return False
+    if (
+            path(
+                newState["positions"][state["current"]],
+                move["new_position"],
+                newState["board"],
+            )
+            is None
+        ):
+            return False
+    return True
 
 
 def test_IA_valid():
@@ -21,4 +77,9 @@ def test_IA_valid():
     nwpi = IA("nwpi","nwpi")
     final = IA('MPST','MPST')
     for ia in listeIA:
-        assert ia.think(stateOfExample)
+        move = ia.think(stateOfExample)
+        assert isValid(move,stateOfExample,move['state'])
+def test_Utils():
+    #pas besoin de tester mes fonctions utils si mes modeles dne font jamais derreurs
+    return False
+test_IA_valid()
